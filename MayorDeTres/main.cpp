f413c8f2b2968aa8b9e1ae7e18646692f74a1c75
magrcruz <magr300@gmail.com>
2019-08-22 20:04:33 -0500

    mayordetres

--- MayorDeTres/main.cpp
+++ MayorDeTres/main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a,b,c,mayor;
+    cout << "Ingrese un numero entero" << endl;
+    cin >> a;
+    cout << "Ingrese otro numero entero" << endl;
+    cin >> b;
+    cout << "Ingrese otro numero entero" << endl;
+    cin >> c;
+    if (a>b)
+        mayor=a;
+    else
+        mayor=b;
+    if (c>mayor)
+        mayor=c;
+    cout << "El mayor numero es: "<< mayor << endl;
     return 0;
 }
