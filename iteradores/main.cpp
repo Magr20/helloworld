29c9a0470ace942c1f5da46d83668846b106716c
magrcruz <magr300@gmail.com>
2019-08-28 14:39:41 -0500

    Tarea completa

--- iteradores/main.cpp
+++ iteradores/main.cpp
@@ -2,91 +2,200 @@
 
 using namespace std;
 
+/**
+Tarea completa, todos las partes funcinan con los valores iniciales de a,b,c los cuales no estan comentados
+Recibe una entrada en a, la cual es ignorada en el segundo problema pero vital para los demás
+Está comentado con dos slash lo que ejecuta cada parte de código comentada entre "/**" y  "* /"
+Para ejecutar una parte del programa colo quitar o comentar las líneas de  "/**" y  "* /"
+PD: se trabaja mejor sin presion
+*/
+
 int main()
 {
-//    int n;
-//    int m;
-//    cin>>n;
-//    /**
-//    m=1;
-//    while(m<n){
-//        m=m*2;
-//    }
-//    if (n==m)
-//        cout << "Es potencia de dos" << endl;
-//    else
-//        cout << "No es potencia de dos"<<endl;
-//    */
-//
-//    /**
-//    programa que diga si es primo o no
-//    */
-//    m=(n/2)+1;
-//    cout << m;
-//    while(m>0){
-//        if (n%m){
-//            m--;
-//        }
-//        else{
-//            cout << "No ";
-//            m=0;
-//        }
-//
-//
-//    }
-//    cout << "es primo";
-
-//    int m;
-//    int n;
-//    int k;
-//    cin>>m;
-//    n=m-1;
-//    while (n>0)
-//    {
-//        k=m/n;
-//        if (n==k*m)
-//        {
-//            cout << k*m;
-//            cout << "no es ";
-//            n=0;
-//        }
-//        else
-//            n--;
-////
-////    }
-//
-//    cout << "compuesto ";
-
-//    int a,b=1,c=0;
-//    cin >>a;
-//    while (b<a)
-//    {
-//        if (c==a)
-//        {
-//            cout << "Es perfecto ";
-//            b=a;
-//        }
-//
-//        else
-//        {
-//            c=c+b;
-//            b++;
-//        }
-//    }
-
-    int a,b,c;
+    int a, b=1, c=0;
     cin>>a;
-    b=a/2;
-    while (b>0)
-    {
-        if (a%b)
-            cout<<a%b;
-        b--;
+    //Indica si es  potencia de dos
+    /**
+    m=1;
+    while(m<n){
+        m=m*2;
     }
+    if (n==m)
+        cout << "Es potencia de dos" << endl;
+    else
+        cout << "No es potencia de dos"<<endl;
+    */
 
+    //Verifica si el número es primo WHILE
+    /**
+    while(b<a){
+        if (!(a%b))
+            c++;
+        b++;
+    }
+    if ((c-1)){
+        cout << "No es primo" << endl;
+    }
+    else
+        cout<<"Es primo"<<endl;
+    */
 
+    //Verifica si el número es primo FOR
+    /**
+    for(b;b<a;b++){
+        if (!(a%b))
+            c++;
+    }
+    if ((c-1)){
+        cout << "No es primo" << endl;
+    }
+    else
+        cout<<"Es primo"<<endl;
+    */
 
+    //Numeros primos del 1 al 100 FOR
+    /**
+    cout <<"Los numeros primos del 1 al 100 son:"<<endl;
+    for (a=1;a<100;a++){
+        for(b=1;b<a;b++){
+            if (!(a%b))
+                c++;
+        }
+        if (!(c-1))
+            cout <<a<<"  ";
+        c=0;
+    }
+    */
 
+    //Numeros primos del 1 al 100 WHILE
+    /**
+    cout <<"Los numeros primos del 1 al 100 son:"<<endl;
+    a=1;
+    while (a<100){
+        b=1;
+        while(b<a){
+            if (!(a%b))
+                c++;
+            b++;
+        }
+        if (!(c-1))
+            cout <<a<<"  ";
+        c=0;
+        a++;
+    }
+    */
 
-    return 0;
+    //Encuentra los numeros perfectos WHILE
+    /**
+    while(b<a){
+        if (!(a%b)){
+            c=c+b;
+        }
+        b++;
+    }
+    if (c==b)
+        cout<<"Es un numero PERFECTO"<<endl;
+    else
+        cout<<"No es un numero PERFECTO"<<endl;
+    */
+
+    //Encuentra los numeros perfectos FOR
+    /**
+    for(b;b<a;b++){
+        if (!(a%b)){
+            c=c+b;
+        }
+    }
+    if (c==b)
+        cout<<"Es un numero PERFECTO"<<endl;
+    else
+        cout<<"No es un numero PERFECTO"<<endl;
+    */
+
+    return 0; //Verifica si el número es primo WHILE
+    /**
+    while(b<a){
+        if (!(a%b))
+            c++;
+        b++;
+    }
+    if ((c-1)){
+        cout << "No es primo" << endl;
+    }
+    else
+        cout<<"Es primo"<<endl;
+    */
+
+    //Verifica si el número es primo FOR
+    /**
+    for(b;b<a;b++){
+        if (!(a%b))
+            c++;
+    }
+    if ((c-1)){
+        cout << "No es primo" << endl;
+    }
+    else
+        cout<<"Es primo"<<endl;
+    */
+
+    //Numeros primos del 1 al 100 FOR
+    /**
+    cout <<"Los numeros primos del 1 al 100 son:"<<endl;
+    for (a=1;a<100;a++){
+        for(b=1;b<a;b++){
+            if (!(a%b))
+                c++;
+        }
+        if (!(c-1))
+            cout <<a<<"  ";
+        c=0;
+    }
+    */
+
+    //Numeros primos del 1 al 100 WHILE
+    /**
+    cout <<"Los numeros primos del 1 al 100 son:"<<endl;
+    a=1;
+    while (a<100){
+        b=1;
+        while(b<a){
+            if (!(a%b))
+                c++;
+            b++;
+        }
+        if (!(c-1))
+            cout <<a<<"  ";
+        c=0;
+        a++;
+    }
+    */
+
+    //Encuentra los numeros perfectos WHILE
+    /**
+    while(b<a){
+        if (!(a%b)){
+            c=c+b;
+        }
+        b++;
+    }
+    if (c==b)
+        cout<<"Es un numero PERFECTO"<<endl;
+    else
+        cout<<"No es un numero PERFECTO"<<endl;
+    */
+
+    //Encuentra los numeros perfectos FOR
+    /**
+    for(b;b<a;b++){
+        if (!(a%b)){
+            c=c+b;
+        }
+    }
+    if (c==b)
+        cout<<"Es un numero PERFECTO"<<endl;
+    else
+        cout<<"No es un numero PERFECTO"<<endl;
+    */
+
 }
