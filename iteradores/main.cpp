bbc64f267615820fe41fb932be23875d1ef695ef
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2019-08-28 09:56:55 -0500

    Tienes que practiar!

--- iteradores/main.cpp
+++ iteradores/main.cpp
@@ -4,14 +4,89 @@ using namespace std;
 
 int main()
 {
-    int n,m=1;
-    cin>>n;
-    while(m<n){
-        m=m*2;
+//    int n;
+//    int m;
+//    cin>>n;
+//    /**
+//    m=1;
+//    while(m<n){
+//        m=m*2;
+//    }
+//    if (n==m)
+//        cout << "Es potencia de dos" << endl;
+//    else
+//        cout << "No es potencia de dos"<<endl;
+//    */
+//
+//    /**
+//    programa que diga si es primo o no
+//    */
+//    m=(n/2)+1;
+//    cout << m;
+//    while(m>0){
+//        if (n%m){
+//            m--;
+//        }
+//        else{
+//            cout << "No ";
+//            m=0;
+//        }
+//
+//
+//    }
+//    cout << "es primo";
+
+//    int m;
+//    int n;
+//    int k;
+//    cin>>m;
+//    n=m-1;
+//    while (n>0)
+//    {
+//        k=m/n;
+//        if (n==k*m)
+//        {
+//            cout << k*m;
+//            cout << "no es ";
+//            n=0;
+//        }
+//        else
+//            n--;
+////
+////    }
+//
+//    cout << "compuesto ";
+
+//    int a,b=1,c=0;
+//    cin >>a;
+//    while (b<a)
+//    {
+//        if (c==a)
+//        {
+//            cout << "Es perfecto ";
+//            b=a;
+//        }
+//
+//        else
+//        {
+//            c=c+b;
+//            b++;
+//        }
+//    }
+
+    int a,b,c;
+    cin>>a;
+    b=a/2;
+    while (b>0)
+    {
+        if (a%b)
+            cout<<a%b;
+        b--;
     }
-    if (n==m)
-        cout << "Es potencia de dos" << endl;
-    else
-        cout << "No es potencia de dos"<<endl;
+
+
+
+
+
     return 0;
 }
