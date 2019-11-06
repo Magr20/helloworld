c498f8ef65edf5189adc2c75ee9f0d2f695012a3
magrcruz <50184830+magrcruz@users.noreply.github.com>
2019-11-06 07:15:29 -0500

    Update Curso.cpp

--- Alumno/src/Curso.cpp
+++ Alumno/src/Curso.cpp
@@ -4,20 +4,34 @@ Curso::Curso()
 {
     //ctor
 }
+Curso::Curso(Curso &o)
+{
+    nombre=o.nombre;
+    codigo=o.codigo;
+    alumnos=o.alumnos;
+    cantidadAlumnos=o.cantidadAlumnos;
+}
+Curso::Curso(string a,string b,Estudiante* puntero, int c)
+{
+    setNombre(a);
+    setCodigo(b);
+    setDireccion(puntero);
+    cantidadAlumnos=c;
+}
 
 Curso::~Curso()
 {
     //dtor
 }
 
-string getNombre();
-string getCodigo();
-Estudiante * getDireccion();
-int getCantidad();
+string Curso::getNombre(){return nombre;}
+string Curso::getCodigo(){return codigo;}
+Estudiante * Curso::getDireccion(){return alumnos;}
+int getCantidad(){return cantidadAlumnos;}
 
-void setNombre(string);
-void setCodigo(string);
-void setDireccion(Estudiante*);
-void setCantidad(int);
+void Curso::setNombre(string a){nombre=a;}
+void Curso::setCodigo(string a){codigo=a;}
+void Curso::setDireccion(Estudiante* a){alumnos=a;}
+void Curso::setCantidad(int a){cantidadAlumnos=a;}
 
-void incrementoEstudiante();
+void Curso::incrementoEstudiante(){cantidadAlumnos+=1;}
